7556ced5bb7274571634162e6f496e06c0773567
scottweisman <sweisman18@gmail.com>
2010-11-30 21:44:48 -0600

    Added a User factory and tests for user show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Scott Weisman"
+  user.email                 "sweisman@example.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end 
