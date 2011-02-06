522e9db6a79f459cd8b9a941bdc74e9e695cd3aa
scottweisman <sweisman18@gmail.com>
2011-02-06 17:46:00 -0600

    User relationships (following/followers)

---
+++ app/models/relationship.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+# Schema version: 20110204011131
+#
+# Table name: relationships
+#
+#  id          :integer         not null, primary key
+#  follower_id :integer
+#  followed_id :integer
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
+class Relationship < ActiveRecord::Base
+  attr_accessible :followed_id
+  
+  #need to specify foreign_key here because rails would assume it was user_id?
+  belongs_to :follower, :foreign_key => "follower_id", :class_name => "User"
+  belongs_to :followed, :foreign_key => "followed_id", :class_name => "User"
+  
+  validates :follower_id, :presence => true
+  validates :followed_id, :presence => true 
+end
